2312d80035b646a2c571938ffaa431d829209a46
Yavuz Efe <75783774+Yefee8@users.noreply.github.com>
2024-01-07 16:08:46 +0300

    fix: footer border length

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <footer className="flex flex-wrap  mt-12 items-center justify-center gap-8 md:justify-between py-16 border-t-2 border-primary/30">
+        <footer className="flex flex-wrap mt-12 items-center justify-center gap-8 md:justify-between py-16 border-t-1 border-primary/30">
             <h1 className="text-4xl font-bold md:ml-16">Geniusula</h1>
 
             <h3 className="text-lg font-medium opacity-70 md:mr-16">Yefee tarafından tasarlandı/kodlandı.</h3>
